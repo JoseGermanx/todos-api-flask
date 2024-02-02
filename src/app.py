fd98dcd8d6734a1e11f91fe9223f34754ac16994
Jose Germán Martínez <jgermanmm@gmail.com>
2024-02-01 22:14:56 -0300

    actualizacion

--- src/app.py
+++ src/app.py
@@ -11,6 +11,12 @@ todos = [
 def index():
     return jsonify(todos)
 
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.get_json()
+    print("Incoming request with the following body", request_body)
+    return 'Response for the POST todo'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
