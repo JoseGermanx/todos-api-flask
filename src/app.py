45880e2eb30d286308563abb1004717e9171a13c
Jose Germán Martínez <jgermanmm@gmail.com>
2024-02-02 00:29:44 -0300

    Agregada función para eliminar tareas

--- src/app.py
+++ src/app.py
@@ -20,6 +20,11 @@ def add_new_todo():
     todos.append(add_todo)
     return jsonify(todos)
 
+@app.route('/delete/<int:position>', methods=['DELETE'])
+def delete_todos(position):
+    todos.pop(position)
+    return jsonify(todos)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
